02d64cf7716c157522cefb451472f6397563cdf1
mrsfreddy <rmogusu123@gmal.com>
2019-11-15 19:21:04 +0200

    Initial commit

---
+++ src/main/java/PingPong.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+
+public class PingPong {
+
+    public ArrayList<Object> runPingPong(int countUpTo){
+        ArrayList<Object> result = new ArrayList<Object>();
+        for (int i = 1; i <= countUpTo; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                result.add("PingPong");
+            } else if (i % 3 == 0){
+                result.add("ping");
+            } else if (i % 5 == 0){
+                result.add("pong");
+            } else {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+
+}
